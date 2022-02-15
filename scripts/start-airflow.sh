83d50d2f93bab9d7f8b08bb985913e2e283acaf6
Sundar Gopal <sundargopal17@gmail.com>
2022-02-15 21:24:35 +0530

    to test my github access cli

--- scripts/start-airflow.sh
+++ scripts/start-airflow.sh
@@ -6,7 +6,7 @@ airflow db init
 # Create User
 airflow users create -e "admin@airflow.com" -f "airflow" -l "airflow" -p "airflow" -r "Admin" -u "airflow"
 
-# Run the scheduler in background
+# Run the scheduler in background ,this format is unix defined one 
 airflow scheduler &> /dev/null &
 
 # Run the web sever in foreground (for docker logs)
