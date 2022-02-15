d462a3777876b2a9eb603ade44ea55d1f3a4bb65
Sundar Gopal <sundargopal17@gmail.com>
2022-02-15 21:13:36 +0530

    build dockerfile is now working where we can customize the airflow settings and config files.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 airflow db init
 airflow users create -e "admin@airflow.com" -f "airflow" -l "airflow" -p "airflow" -r "Admin" -u "airflow"
+pip install apache-airflow-providers-slack
 exec airflow webserver
